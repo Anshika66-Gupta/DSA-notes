ce178a2a7dc6e2cba1778ee98190ed33c7b60eef
“Anshika <ansgupta.01@gmail.com>
2023-12-28 19:45:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//var a = 12;
+//var b = 13;
+//var c = a + b;
+//document.write(c)
+
+//const fruits = [];
+//fruits.push("banana", "apple", "orange", 12);
+////console.log(fruits.length)
+//document.write(fruits.length)
+
+
+//const fruits = ["banana", "apple", "orange"];
+//let size = fruits.length;
+//document.write(size);
+
+
+//const fruits = ["banana", "apple", "orange"];
+//document.getElementById("first").innerHTML = fruits.join("-")
+
+//const fruits = ["banana", "apple", "orange"];
+//document.getElementById("first").innerHTML = fruits;
+//fruits.pop();
+//document.getElementById("first").innerHTML = fruits;
+
