562c135c55b5ce87e767ed84a5a15204f9cd8c3c
Anshika Gupta <63110987+Anshika66-Gupta@users.noreply.github.com>
2023-12-28 21:08:52 +0530

    28th

--- index.js
+++ index.js
@@ -24,3 +24,94 @@
 //document.getElementById("first").innerHTML = fruits;
 
 //28-12-2023
+
+//Stack example:-
+class Stack {
+    Constructor(){
+        this.items = []; 
+    } 
+    //push element
+    push(element){
+        this.items.push(element);
+    }
+    //pop element
+    pop(){
+        if(this.items.length === 0) {
+            return "Underflow";
+        }
+        return this.items.pop();
+    }
+    peek(){
+        return this.items[this.items.length -1];
+    }
+    isEmpty(){
+        return this.items.length ===0;
+    }
+    
+    printStack(){
+        let str = "";
+        for (let i = 0; i < this.items.length; i++){
+            str += this.items[i] + ""
+        }
+        return str;
+    }
+}
+
+let stack = new Stack();
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log("Stack elements: ", stack.printStack());
+console.log("Popped elements: ", stack.pop());
+console.log("Top elements: ", stack.peek());
+console.log("Is stack empty ", stack.isEmpty());
+
+
+//queue example
+
+class Queue {
+    Constructor(){
+        this.items = []; 
+    } 
+    //enqueue element
+    enqueue(element){
+        this.items.push(element);
+    }
+    //pop element
+    dequeue(){
+        if(this.isEmpty()) {
+            return "Underflow";
+        }
+        return this.items.shift();
+    }
+front(){
+        if(this.isEmpty()) {
+            return "No elements available";
+        }
+        return this.items[0];
+    }
+    isEmpty(){
+        return this.items.length===0;
+    }
+    
+    printQueue(){
+        let str = "";
+        for (let i = 0; i < this.items.length; i++){
+            str += this.items[i] + " ";
+        }
+        return str;
+    }
+}
+
+let queue = new Stack();
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+
+console.log("queue elements: ", queue.printQueue());
+console.log("dequeue elements: ", queue.dequeue());
+console.log("Front elements: ", queue.front());
+console.log("Is queue empty ", queue.isEmpty());
